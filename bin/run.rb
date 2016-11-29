3719fd28eafe03bc50b08c51b55b34c161734ccc
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-29 16:12:44 -0500

    Works for character and movie

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,13 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+user_selection = selection
+if user_selection == "character"
+	character = get_character_from_user
+	show_character_movies(character)
+else
+
+	movie_selected = get_movie_from_user
+	puts get_opening_crawl_from_api(movie_selected)
+
+end
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,29 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+  films_array = []
+  character_hash["results"].each do |char_array|
+     if char_array["name"].downcase == character
+      films_array = char_array["films"]
+     #binding.pry
+    end
+  end
+  films_array
+end
+
+def get_opening_crawl_from_api(movie_selected)
+
+  all_movie = RestClient.get('http://swapi.co/api/films/')
+  movie_hash = JSON.parse(all_movie)
+
+  movie_hash["results"].each do |movie|
+    if movie["title"].downcase == movie_selected
+      return movie["opening_crawl"]
+    end
+  end
+  "That is not a Star Wars movie!"
+end
+
   
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
@@ -16,6 +39,20 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+
+def get_film_info (films_array)
+  #binding.pry
+  if films_array == []
+    puts "Your character was not found in Star Wars."
+  else
+    films_array.collect do |film|
+      #binding.pry
+      film_info = RestClient.get(film)
+      film_hash = JSON.parse(film_info)
+      film_hash["title"]
+    end
+  end
 end
 
 def parse_character_movies(films_hash)
@@ -24,7 +61,8 @@ end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  puts get_film_info(films_hash)
+
 end
 
 ## BONUS
